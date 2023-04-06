d6e70c7ef90d07cb05e53095906e2d53eb5a43f5
Phillip Taylor <phillip.taylor@transformuk.com>
2023-04-06 15:45:34 +0100

    DPSPS-1140: Try and override user.home

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 # Set ACP Artefactory as the proxy for dependency resolution
 echo -en "realm=Artifactory Realm\nhost=artifactory.digital.homeoffice.gov.uk\nuser=$ARTIFACTORY_USERNAME\npassword=$ARTIFACTORY_PASSWORD" > /app/.ivy2/.credentials
-export SBT_OPTS="-Dsbt.override.build.repos=true -Dsbt.ivy.home=/app/.ivy2"
+export SBT_OPTS="-Duser.home=/app -Dsbt.override.build.repos=true -Dsbt.ivy.home=/app/.ivy2"
 export SBT_CREDENTIALS="/app/.ivy2/.credentials"
 
 exec "$@"
