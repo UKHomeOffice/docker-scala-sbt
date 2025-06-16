81e9088f441b94208da3ed704d51c1b7f0637079
Phillip Taylor <phillip.taylor@transformuk.com>
2025-06-16 14:29:12 +0100

    DPSPS-1701: Deal with some repository problems on newer builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,6 @@ export PATH=$JAVA_HOME/bin:$PATH
 # Set ACP Artefactory as the proxy for dependency resolution
 echo -en "realm=Artifactory Realm\nhost=artifactory.digital.homeoffice.gov.uk\nuser=$ARTIFACTORY_USERNAME\npassword=$ARTIFACTORY_PASSWORD" > /app/.ivy2/.credentials
 export SBT_CREDENTIALS="/app/.ivy2/.credentials"
-export SBT_OPTS="-Duser.home=/app -Dsbt.override.build.repos=true -Dsbt.ivy.home=/app/.ivy2"
+export SBT_OPTS="-Duser.home=/app -Dsbt.override.build.repos=${OVERRIDE_BUILD_REPOS:-true} -Dsbt.ivy.home=/app/.ivy2"
 
 exec "$@"
